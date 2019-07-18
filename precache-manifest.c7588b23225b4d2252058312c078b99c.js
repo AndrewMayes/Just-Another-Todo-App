5b4b3133be2eb34382b7e3bfa50f75d627ffdff5
AndrewMayes <andrewmayes757@gmail.com>
2019-07-18 18:43:28 -0400

    Updates

---
+++ precache-manifest.c7588b23225b4d2252058312c078b99c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee196fe499ce0e703e7bd6eae2585dc",
+    "url": "/Just-Another-Todo-App/index.html"
+  },
+  {
+    "revision": "f84ab23701f8b7a26895",
+    "url": "/Just-Another-Todo-App/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "75225eb4cbd20fb4bdc6",
+    "url": "/Just-Another-Todo-App/static/js/2.55ab8f70.chunk.js"
+  },
+  {
+    "revision": "f84ab23701f8b7a26895",
+    "url": "/Just-Another-Todo-App/static/js/main.797200cf.chunk.js"
+  },
+  {
+    "revision": "17f843201d8ecea6d9ce",
+    "url": "/Just-Another-Todo-App/static/js/runtime~main.d2fdee19.js"
+  }
+]);
